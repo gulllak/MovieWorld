5f888251dd9ade6273f21368b4f383d2baa822fb
i11usion <igoryunnn@yandex.ru>
2023-11-28 20:02:05 +0300

    refactor: изменение алгоритма нахождения общих фильмов

--- src/main/java/ru/yandex/practicum/filmorate/storage/like/impl/LikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like/impl/LikeDbStorage.java
@@ -43,7 +43,7 @@ public class LikeDbStorage implements LikeStorage {
     }
 
     @Override
-    public List<Long> getCommonFilmIds(Long userId, Long friendId){
+    public List<Long> getCommonFilmIds(Long userId, Long friendId) {
         String sqlQuery = "SELECT l1.film_id\n" +
                 "FROM likes AS l1\n" +
                 "INNER JOIN likes AS l2 ON l1.film_id = l2.film_id AND l1.user_id <> l2.user_id\n" +
